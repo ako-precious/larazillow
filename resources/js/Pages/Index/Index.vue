0527d48bf9f350cd71ffd9769f4fbfe89bede776
AKO PRECIOUS <akoprecious58@gmail.com>
2023-08-06 15:17:12 -0400

    back to learning Vue

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,12 +1,17 @@
 <template>
-    <MainLayout>
+    
         <div>Index</div>
         <Link href="/show">Show Pages</Link>
         <div>The message is {{ message }}</div>
-    </MainLayout>
+   
 </template>
 <script setup>
 import { Link } from "@inertiajs/vue3";
-import MainLayout from "../../Layouts/MainLayout.vue";
 defineProps(["mesage"]);
+</script>
+<script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+export default {
+   layout: MainLayout
+}
 </script>
